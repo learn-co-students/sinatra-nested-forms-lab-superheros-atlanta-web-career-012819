f45c9630457d3af3d606b4f34d684f826dcfa59a
Megan Fago <meganlaynefago@gmail.com>
2019-02-20 15:59:35 -0500

    Done.

---
+++ app/models/super_hero.rb
@@ -0,0 +1,19 @@
+class SuperHero
+
+  attr_reader :name, :power, :bio, :team
+
+@@all = []
+
+  def initialize(name, power, bio, team)
+    @name = name
+    @power = power
+    @bio = bio
+    @team = team
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
